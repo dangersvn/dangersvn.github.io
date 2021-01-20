ab3d6c42d9563cc467d1480e89d7f744a0ae1004
haidang063@gmail.com <haidang063@gmail.com>
2021-01-19 21:41:13 -0600

    Add banking account page

---
+++ bankingaccount/bank.js
@@ -0,0 +1,39 @@
+
+var bankModule = function () {
+  let balance = 0;
+  let accountName;
+  let createBankAccount = function (accName, deposit) {
+    balance = deposit;
+    accountName = accName;
+
+    return {
+      accountName: accName,
+      balance: deposit
+    }
+  }
+  return { createBankAccount };
+}
+
+var accountInfoList = [];
+window.onload = function () {  
+  createAccountButton.onclick = createAccount;
+}
+
+function createAccount() {  
+  let accName = accountNameInput.value;
+  let deposit = depositInput.value;
+   //validate
+
+   let acc = bankModule().createBankAccount(accName, +deposit);
+   accountInfoList.push(acc);
+   displayAccounts();
+}
+
+function displayAccounts() {
+  let txtVal="";
+  //display list accounts
+  for(let acc of accountInfoList) {
+    txtVal += `Account name: ${acc.accountName} Balance: ${acc.balance} \n`;
+  }
+  textareaAccounts.value = txtVal; 
+}
